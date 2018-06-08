738701027546b5c69b4feabb0094f2cf09e1ee27
Sabrina Kennedy <skenne21@gmail.com>
2018-06-08 12:01:26 -0600

    Add all the days to the calendar with the hoilday information

--- src/components/day/Day.js
+++ src/components/day/Day.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Day = ({ date, holiday }) => {
+  console.log(holiday)
   return(
     <div>
-      
+      <h3>{date}</h3>
+      <p>{holiday.length ? holiday[0].holiday : ''}</p>
     </div>
   )
 }
